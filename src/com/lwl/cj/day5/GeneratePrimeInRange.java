1067510bc6ada313f89e218a48d135edd34c412f
Lakshman <tanvi.avula@outlook.com>
2020-04-27 10:32:32 +0530

    Generate prime number between range

--- src/com/lwl/cj/day5/GeneratePrimeInRange.java
+++ src/com/lwl/cj/day5/GeneratePrimeInRange.java
@@ -1,19 +1,13 @@
 package com.lwl.cj.day5;
-
 import java.util.Scanner;
-
 public class GeneratePrimeInRange {
-
 		public static void main(String[] args) {
-		
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter the lb value:");
 			int lb = sc.nextInt();
 			System.out.println("Enter the ub value:");
 			int ub = sc.nextInt();
-			
 			for(int k=lb;k<=ub;k++) {
-				
 				int num = k;
 				boolean flag = true;
 				for (int i = 2; i <= num / 2; i++) {
@@ -22,12 +16,10 @@ public class GeneratePrimeInRange {
 						break;
 					}
 				}
-				
 				if(flag) {
 					System.out.println(num);
 				}
 			}
 			sc.close();
-			
 		}
 }
